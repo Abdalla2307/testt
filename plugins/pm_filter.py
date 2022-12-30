c17019954a0be0bd32d72b54e365aea201fab7fc
Abdalla2307 <99101601+Abdalla2307@users.noreply.github.com>
2022-12-30 04:50:15 +0200

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -217,7 +217,7 @@ async def next_page(bot, query):
         ]
     btn.insert(1,
         [
-            InlineKeyboardButton(text="⚡𝐂𝐡𝐞𝐜𝐤 𝐏𝐌⚡", text="hello", show_alert=True)
+            InlineKeyboardButton(text="⚡𝐂𝐡𝐞𝐜𝐤 𝐏𝐌⚡")
         ], [
     btn.insert(10,
         [
@@ -1296,7 +1296,7 @@ async def auto_filter(client, msg, spoll=False):
         ]
     btn.insert(1,
         [
-            InlineKeyboardButton(text="⚡𝐂𝐡𝐞𝐜𝐤 𝐏𝐌⚡", text="hello", show_alert=True)
+            InlineKeyboardButton(text="⚡𝐂𝐡𝐞𝐜𝐤 𝐏𝐌⚡"
         ], [
     btn.insert(10,
         [
